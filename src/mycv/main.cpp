4b2b11e8e48ff3fd2f6ed8bcab2fdf68e5b88a53
babywade <yixiaofei@stu.xjtu.edu.cn>
2018-03-06 21:33:00 +0800

    1

--- src/mycv/main.cpp
+++ src/mycv/main.cpp
@@ -1,9 +1,8 @@
 //-----------------------------------【程序说明】----------------------------------------------
-//  程序名称:：【OpenCV入门教程之四】 创建Trackbar&图像对比度、亮度值调整 配套博文源码
-// VS2010版  OpenCV版本：2.4.8
-//  2014年3月18 日 Create by 浅墨
+//            程序名称:：【OpenCV入门教程之八】线性滤波专场：方框滤波、均值滤波与高斯滤波 配套源码
+//            开发所用OpenCV版本：2.4.8
+//            2014年3月31 日 Create by 浅墨
 //------------------------------------------------------------------------------------------------
-
 
 //-----------------------------------【头文件包含部分】---------------------------------------
 //     描述：包含程序所依赖的头文件
@@ -20,78 +19,116 @@ using namespace std;
 using namespace cv;
 
 
-//-----------------------------------【全局函数声明部分】--------------------------------------
-//     描述：全局函数声明
-//-----------------------------------------------------------------------------------------------
-static void ContrastAndBright(int, void *);
-
 //-----------------------------------【全局变量声明部分】--------------------------------------
 //     描述：全局变量声明
 //-----------------------------------------------------------------------------------------------
-int g_nContrastValue; //对比度值
-int g_nBrightValue;  //亮度值
-Mat g_srcImage,g_dstImage;
+Mat g_srcImage,g_dstImage1,g_dstImage2,g_dstImage3;//存储图片的Mat类型
+int g_nBoxFilterValue=3;  //方框滤波参数值
+int g_nMeanBlurValue=3;  //均值滤波参数值
+int g_nGaussianBlurValue=3;  //高斯滤波参数值
+
+
+//-----------------------------------【全局函数声明部分】--------------------------------------
+//     描述：全局函数声明
+//-----------------------------------------------------------------------------------------------
+//轨迹条的回调函数
+static void on_BoxFilter(int, void *);            //方框滤波
+static void on_MeanBlur(int, void *);           //均值滤波
+static void on_GaussianBlur(int, void *);                    //高斯滤波
+
+
+
 //-----------------------------------【main( )函数】--------------------------------------------
 //     描述：控制台应用程序的入口函数，我们的程序从这里开始
 //-----------------------------------------------------------------------------------------------
 int main(  )
 {
-       //改变控制台前景色和背景色
-       system("color5F");
+       //改变console字体颜色
+       system("color5E");
 
-       //读入用户提供的图像
-       g_srcImage= imread( "test.jpg");
-              if(!g_srcImage.data ) { printf("Oh，no，读取g_srcImage图片错误~！\n"); return false; }
-       g_dstImage= Mat::zeros( g_srcImage.size(), g_srcImage.type() );
+       //载入原图
+       g_srcImage= imread( "1.png", 1 );
+       if(!g_srcImage.data ) { printf("Oh，no，读取srcImage错误~！\n"); return false; }
 
-       //设定对比度和亮度的初值
-       g_nContrastValue=80;
-       g_nBrightValue=80;
+       //克隆原图到三个Mat类型中
+       g_dstImage1= g_srcImage.clone( );
+       g_dstImage2= g_srcImage.clone( );
+       g_dstImage3= g_srcImage.clone( );
 
+       //显示原图
+       namedWindow("【<0>原图窗口】", 1);
+       imshow("【<0>原图窗口】",g_srcImage);
+
+
+       //=================【<1>方框滤波】==================
        //创建窗口
-       namedWindow("【效果图窗口】", 0);
-
+       namedWindow("【<1>方框滤波】", 1);
        //创建轨迹条
-       createTrackbar("对比度：", "【效果图窗口】",&g_nContrastValue,300,ContrastAndBright );
-       createTrackbar("亮   度：","【效果图窗口】",&g_nBrightValue,200,ContrastAndBright );
+       createTrackbar("内核值：", "【<1>方框滤波】",&g_nBoxFilterValue, 40,on_BoxFilter );
+       on_MeanBlur(g_nBoxFilterValue,0);
+       imshow("【<1>方框滤波】", g_dstImage1);
+       //================================================
 
-       //调用回调函数
-       ContrastAndBright(g_nContrastValue,0);
-       ContrastAndBright(g_nBrightValue,0);
+       //=================【<2>均值滤波】==================
+       //创建窗口
+       namedWindow("【<2>均值滤波】", 1);
+       //创建轨迹条
+       createTrackbar("内核值：", "【<2>均值滤波】",&g_nMeanBlurValue, 40,on_MeanBlur );
+       on_MeanBlur(g_nMeanBlurValue,0);
+       //================================================
+
+       //=================【<3>高斯滤波】=====================
+       //创建窗口
+       namedWindow("【<3>高斯滤波】", 1);
+       //创建轨迹条
+       createTrackbar("内核值：", "【<3>高斯滤波】",&g_nGaussianBlurValue, 40,on_GaussianBlur );
+       on_GaussianBlur(g_nGaussianBlurValue,0);
+       //================================================
+
 
        //输出一些帮助信息
        cout<<endl<<"\t嗯。好了，请调整滚动条观察图像效果~\n\n"
-                     <<"\t按下“q”键时，程序退出~!\n"
-                     <<"\n\n\t\t\t\tby浅墨";
+              <<"\t按下“q”键时，程序退出~!\n"
+              <<"\n\n\t\t\t\tby浅墨";
 
        //按下“q”键时，程序退出
-   while(char(waitKey(1)) != 'q') {}
+       while(char(waitKey(1))!= 'q') {}
+
        return 0;
 }
 
 
-//-----------------------------【ContrastAndBright( )函数】------------------------------------
-//     描述：改变图像对比度和亮度值的回调函数
+//-----------------------------【on_BoxFilter( )函数】------------------------------------
+//     描述：方框滤波操作的回调函数
 //-----------------------------------------------------------------------------------------------
-static void ContrastAndBright(int, void *)
+static void on_BoxFilter(int, void *)
 {
+       //方框滤波操作
+       boxFilter(g_srcImage, g_dstImage1, -1,Size( g_nBoxFilterValue+1, g_nBoxFilterValue+1));
+       //显示窗口
+       imshow("【<1>方框滤波】", g_dstImage1);
+}
 
-       //创建窗口
-       namedWindow("【原始图窗口】", 0);
 
-       //三个for循环，执行运算 g_dstImage(i,j) =a*g_srcImage(i,j) + b
-       for(int y = 0; y < g_srcImage.rows; y++ )
-       {
-              for(int x = 0; x < g_srcImage.cols; x++ )
-              {
-                     for(int c = 0; c < 3; c++ )
-                     {
-                            g_dstImage.at<Vec3b>(y,x)[c]= saturate_cast<uchar>( (g_nContrastValue*0.01)*(g_srcImage.at<Vec3b>(y,x)[c] ) + g_nBrightValue );
-                     }
-              }
-       }
+//-----------------------------【on_MeanBlur( )函数】------------------------------------
+//     描述：均值滤波操作的回调函数
+//-----------------------------------------------------------------------------------------------
+static void on_MeanBlur(int, void *)
+{
+       //均值滤波操作
+       blur(g_srcImage, g_dstImage2, Size( g_nMeanBlurValue+1, g_nMeanBlurValue+1),Point(-1,-1));
+       //显示窗口
+       imshow("【<2>均值滤波】", g_dstImage2);
+}
 
-       //显示图像
-       imshow("【原始图窗口】", g_srcImage);
-       imshow("【效果图窗口】", g_dstImage);
+
+//-----------------------------【on_GaussianBlur( )函数】------------------------------------
+//     描述：高斯滤波操作的回调函数
+//-----------------------------------------------------------------------------------------------
+static void on_GaussianBlur(int, void *)
+{
+       //高斯滤波操作
+       GaussianBlur(g_srcImage, g_dstImage3, Size( g_nGaussianBlurValue*2+1,g_nGaussianBlurValue*2+1 ), 0, 0);
+       //显示窗口
+       imshow("【<3>高斯滤波】", g_dstImage3);
 }
