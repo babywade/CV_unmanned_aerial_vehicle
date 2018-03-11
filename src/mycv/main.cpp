19ff1d0a3e3e0fc8fe0adba554a53a5f47f444e8
babywade <yixiaofei@stu.xjtu.edu.cn>
2018-03-11 21:54:27 +0800

    Remap

--- src/mycv/main.cpp
+++ src/mycv/main.cpp
@@ -1,8 +1,10 @@
 //-----------------------------------【程序说明】----------------------------------------------
-//      程序名称:：《【OpenCV入门教程之十六】OpenCV角点检测之Harris角点检测》 博文配套源码
+//      程序名称:：《【OpenCV入门教程之十七】OpenCV重映射 & SURF特征点检测合辑 》 博文配套源码 之【SURF特征点检测】
 //      开发所用IDE版本：Visual Studio 2010
 //      开发所用OpenCV版本：   2.4.9
-//      2014年6月8日 Created by 浅墨
+//      2014年6月15日 Created by 浅墨
+//      配套博文链接： http://blog.csdn.net/poem_qianmo/article/details/30974513
+//      PS:程序结合配合博文学习效果更佳
 //      浅墨的微博：@浅墨_毛星云 http://weibo.com/1723155442
 //      浅墨的知乎：http://www.zhihu.com/people/mao-xing-yun
 //      浅墨的豆瓣：http://www.douban.com/people/53426472/
@@ -11,117 +13,75 @@
 //-----------------------------------【头文件包含部分】---------------------------------------
 //      描述：包含程序所依赖的头文件
 //----------------------------------------------------------------------------------------------
-#include <opencv2/opencv.hpp>
+#include "opencv2/core/core.hpp"
+#include "opencv2/features2d/features2d.hpp"
 #include "opencv2/highgui/highgui.hpp"
-#include "opencv2/imgproc/imgproc.hpp"
+//#include "opencv2/nonfree/nonfree.hpp"
+#include <iostream>
+
 
 //-----------------------------------【命名空间声明部分】--------------------------------------
-//      描述：包含程序所使用的命名空间
+//          描述：包含程序所使用的命名空间
 //-----------------------------------------------------------------------------------------------
 using namespace cv;
-using namespace std;
-
-//-----------------------------------【宏定义部分】--------------------------------------------
-//  描述：定义一些辅助宏
-//------------------------------------------------------------------------------------------------
-#define WINDOW_NAME1 "【程序窗口1】"        //为窗口标题定义的宏
-#define WINDOW_NAME2 "【程序窗口2】"        //为窗口标题定义的宏
-
-//-----------------------------------【全局变量声明部分】--------------------------------------
-//      描述：全局变量声明
-//-----------------------------------------------------------------------------------------------
-Mat g_srcImage, g_srcImage1,g_grayImage;
-int thresh = 30; //当前阈值
-int max_thresh = 175; //最大阈值
-
 
 //-----------------------------------【全局函数声明部分】--------------------------------------
-//      描述：全局函数声明
+//          描述：全局函数的声明
 //-----------------------------------------------------------------------------------------------
-void on_CornerHarris( int, void* );//回调函数
-static void ShowHelpText();
+static void ShowHelpText( );//输出帮助文字
 
 //-----------------------------------【main( )函数】--------------------------------------------
-//      描述：控制台应用程序的入口函数，我们的程序从这里开始执行
+//   描述：控制台应用程序的入口函数，我们的程序从这里开始执行
 //-----------------------------------------------------------------------------------------------
 int main( int argc, char** argv )
 {
     //【0】改变console字体颜色
-    system("color 3F");
+    system("color 2F");
 
     //【0】显示帮助文字
-    ShowHelpText();
+    ShowHelpText( );
 
-    //【1】载入原始图并进行克隆保存
-    g_srcImage = imread( "ShuDianXian2.jpg", 1 );
-     if(!g_srcImage.data ) { printf("读取图片错误，请确定目录下是否有imread函数指定的图片存在~！ \n"); return false; }
-     imshow("原始图",g_srcImage);
-    g_srcImage1=g_srcImage.clone( );
+    //【1】载入源图片并显示
+    Mat srcImage1 = imread("book1.jpg", 1 );
+    Mat srcImage2 = imread("book2.jpg", 1 );
+    if( !srcImage1.data || !srcImage2.data )//检测是否读取成功
+    { printf("读取图片错误，请确定目录下是否有imread函数指定名称的图片存在~！ \n"); return false; }
+    imshow("原始图1",srcImage1);
+    imshow("原始图2",srcImage2);
 
-    //【2】存留一张灰度图
-    cvtColor( g_srcImage1, g_grayImage, CV_BGR2GRAY );
+    //【2】定义需要用到的变量和类
+    int minHessian = 400;//定义SURF中的hessian阈值特征点检测算子
+    SurfFeatureDetector detector( minHessian );//定义一个SurfFeatureDetector（SURF） 特征检测类对象
+    std::vector<KeyPoint> keypoints_1, keypoints_2;//vector模板类是能够存放任意类型的动态数组，能够增加和压缩数据
 
-    //【3】创建窗口和滚动条
-    namedWindow( WINDOW_NAME1, CV_WINDOW_AUTOSIZE );
-    createTrackbar( "阈值: ", WINDOW_NAME1, &thresh, max_thresh, on_CornerHarris );
+    //【3】调用detect函数检测出SURF特征关键点，保存在vector容器中
+    detector.detect( srcImage1, keypoints_1 );
+    detector.detect( srcImage2, keypoints_2 );
 
-    //【4】调用一次回调函数，进行初始化
-    on_CornerHarris( 0, 0 );
+    //【4】绘制特征关键点
+    Mat img_keypoints_1; Mat img_keypoints_2;
+    drawKeypoints( srcImage1, keypoints_1, img_keypoints_1, Scalar::all(-1), DrawMatchesFlags::DEFAULT );
+    drawKeypoints( srcImage2, keypoints_2, img_keypoints_2, Scalar::all(-1), DrawMatchesFlags::DEFAULT );
+
+    //【5】显示效果图
+    imshow("特征点检测效果图1", img_keypoints_1 );
+    imshow("特征点检测效果图2", img_keypoints_2 );
 
     waitKey(0);
-    return(0);
+    return 0;
 }
 
-//-----------------------------------【on_HoughLines( )函数】--------------------------------
-//      描述：回调函数
-//----------------------------------------------------------------------------------------------
-
-void on_CornerHarris( int, void* )
-{
-    //---------------------------【1】定义一些局部变量-----------------------------
-    Mat dstImage;//目标图
-    Mat normImage;//归一化后的图
-    Mat scaledImage;//线性变换后的八位无符号整型的图
-
-    //---------------------------【2】初始化---------------------------------------
-    //置零当前需要显示的两幅图，即清除上一次调用此函数时他们的值
-    dstImage = Mat::zeros( g_srcImage.size(), CV_32FC1 );
-    g_srcImage1=g_srcImage.clone( );
-
-    //---------------------------【3】正式检测-------------------------------------
-    //进行角点检测
-    cornerHarris( g_grayImage, dstImage, 2, 3, 0.04, BORDER_DEFAULT );
-
-    // 归一化与转换
-    normalize( dstImage, normImage, 0, 255, NORM_MINMAX, CV_32FC1, Mat() );
-    convertScaleAbs( normImage, scaledImage );//将归一化后的图线性变换成8位无符号整型
-
-    //---------------------------【4】进行绘制-------------------------------------
-    // 将检测到的，且符合阈值条件的角点绘制出来
-    for( int j = 0; j < normImage.rows ; j++ )
-    { for( int i = 0; i < normImage.cols; i++ )
-    {
-        if( (int) normImage.at<float>(j,i) > thresh+80 )
-        {
-            circle( g_srcImage1, Point( i, j ), 5,  Scalar(10,10,255), 2, 8, 0 );
-            circle( scaledImage, Point( i, j ), 5,  Scalar(0,10,255), 2, 8, 0 );
-        }
-    }
-    }
-    //---------------------------【4】显示最终效果---------------------------------
-    imshow( WINDOW_NAME1, g_srcImage1 );
-    imshow( WINDOW_NAME2, scaledImage );
-
-}
 
 //-----------------------------------【ShowHelpText( )函数】----------------------------------
-//      描述：输出一些帮助信息
+//          描述：输出一些帮助信息
 //----------------------------------------------------------------------------------------------
-static void ShowHelpText()
+void ShowHelpText()
 {
     //输出一些帮助信息
-    printf("\n\n\n\t\t\t【欢迎来到Harris角点检测示例程序~】\n\n");
-    printf("\n\n\n\t请调整滚动条观察图像效果~\n\n");
-    printf("\n\n\t\t\t\t\t\t\t\t by浅墨"
-        );
+    printf("\n\n\n\t欢迎来到【SURF特征点检测】示例程序~\n\n");
+    printf("\t当前使用的OpenCV版本为 OpenCV "CV_VERSION);
+    printf( "\n\n\t按键操作说明: \n\n"
+                "\t\t键盘按键任意键- 退出程序\n\n"
+                "\n\n\t\t\t\t\t\t\t\t by浅墨\n\n\n");
+
 }
